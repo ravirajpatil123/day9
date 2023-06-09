06e6e626889286e540064afbd97eb88f5fbd88ae
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-09 20:41:36 +0530

    1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-import {sum} from "./calc.js";
-sum(1,1);
-console.log("hello world");
+import { MongoClient } from "mongodb";
+
+async function main(){
+    const uri = "mongodb://localhost:27017";
+    const client = new MongoClient(uri);
+    const db = client.db("mydb");
+    const messageColl =  db.collection("message");
+    await messageColl.insertOne({message :"Hi I am Ravi"});
+
+    await client.close();
+    console.log("record Added!")
+}
+
+main();
